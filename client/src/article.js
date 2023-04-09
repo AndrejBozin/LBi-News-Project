8c42ce73c7db13821a6fd2707c9307f9d1c6cce3
Andrej Bozin <82775190+AndrejBozin@users.noreply.github.com>
2023-04-09 18:48:51 -0400

    Modified CSS and HTML to look nice

--- client/src/article.js
+++ client/src/article.js
@@ -1,34 +1,34 @@
 const Article = (props) => {
     const {title, description, content, url, image, publishedAt, source} = props.article;
     return (
-        <div className="card">
-      <div className="user_img">
-        <img
-          src="https://images.unsplash.com/flagged/photo-1559517207-2ad4420184d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-          alt=""
-        />
-      </div>
-
-      <div className="user_info">
-        <h1>Jack Nelson</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </p>
-        <div className="row">
-          <div className="follow">
-            <p>Followers</p>
-            <h1>963</h1>
-          </div>
-          <div className="follower">
-            <p>Following</p>
-            <h1>224</h1>
-          </div>
-          <div className="btn">
-            <a href="">Follow</a>
-          </div>
+      <div className="card">
+        <div className="article_img">
+            <img
+            src={image}
+            alt=":("
+            />
         </div>
-      </div>
+
+        <div className="article_info">
+            <h1>{title}</h1>
+            <p>
+                {description}
+            </p>
+            <div className="row">
+            {/* <div className="follow">
+                <p>Followers</p>
+                <h1>963</h1>
+            </div>
+            <div className="follower">
+                <p>Following</p>
+                <h1>224</h1>
+            </div> */}
+            <div className="btn">
+                <a href={url}>Link</a>
+            </div>
+            </div>
+        </div>
+
     </div>
     )
 }
